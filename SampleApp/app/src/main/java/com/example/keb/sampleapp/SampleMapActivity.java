2d23cc1b26a546db337d1b00a0961f358c5bdd32
sinha08 <aks.sinha6@gmail.com>
2017-06-25 12:49:13 +0530

    Ignoring permission warning

--- SampleApp/app/src/main/java/com/example/keb/sampleapp/SampleMapActivity.java
+++ SampleApp/app/src/main/java/com/example/keb/sampleapp/SampleMapActivity.java
@@ -101,17 +101,8 @@ public class SampleMapActivity extends FragmentActivity implements OnMapReadyCal
         }
     }
 
+	@SuppressWarnings("MissingPermission")
     private void getLastLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
         mFusedLocationClient.getLastLocation()
                 .addOnCompleteListener(this, new OnCompleteListener<Location>() {
                     @Override
